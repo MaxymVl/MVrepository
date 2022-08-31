14d91211f491ef0116554b172849ec2ed44bc249
MaxymVl <111682894+MaxymVl@users.noreply.github.com>
2022-08-31 16:16:38 +0300

    Update Home_work2_5_str.py

--- Home_work2_5_str.py
+++ Home_work2_5_str.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-l=int(input("Введите трехзначное число: "))
-sm=str(l)
-a=int(sm[0])
-b=int(sm[1])
-c=int(sm[2])
-
+l = int(input("Введите трехзначное число: "))
+sm = str(l)
+a = int(sm[0])
+b = int(sm[1])
+c = int(sm[2])
 print(f"Сумма цифр числа:", a+b+c)
