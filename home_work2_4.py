a7f6c61d495a97eaf43c7d98571d7964035ed5f0
MaxymVl <111682894+MaxymVl@users.noreply.github.com>
2022-08-31 16:18:26 +0300

    Update home_work2_4.py

--- home_work2_4.py
+++ home_work2_4.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 from datetime import timedelta
 import datetime
-year=timedelta(365)
-name1=str(input("Введите имя, фамилию: \n"))
-name=str(name1)
-db=input("Введите дату рождения:  (yyyy-mm-dd) \n")
-dd=input("Введите дату смерти: (yyyy-mm-dd) \n")
-d_birth=datetime.datetime.strptime(db, '%Y-%m-%d').date()
-d_death=datetime.datetime.strptime(dd, '%Y-%m-%d').date()
-age_d=(d_death-d_birth)
-age_day=int(age_d.days)
-age_year=age_d.days//365
+year = timedelta(365)
+name1 = str(input("Введите имя, фамилию: \n"))
+name = str(name1)
+db = input("Введите дату рождения:  (yyyy-mm-dd) \n")
+dd = input("Введите дату смерти: (yyyy-mm-dd) \n")
+d_birth = datetime.datetime.strptime(db, '%Y-%m-%d').date()
+d_death = datetime.datetime.strptime(dd, '%Y-%m-%d').date()
+age_d = (d_death-d_birth)
+age_day = int(age_d.days)
+age_year = age_d.days//365
 print(f'Name: {name}')
 print(f'Age: {age_year}')
