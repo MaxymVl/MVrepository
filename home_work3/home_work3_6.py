b18c6ac0cc09e10b67318be72db06aef11d3b614
MaxymVl <111682894+MaxymVl@users.noreply.github.com>
2022-09-08 17:31:19 +0300

    Update home_work3_6.py

--- home_work3/home_work3_6.py
+++ home_work3/home_work3_6.py
@@ -1,10 +1,7 @@
-from re import S
-
-
-def my_sum(*args, start):
+def my_sum(*args, start=0):
     args_sum = sum(args)
     y = int(start)
     p = args_sum + y
     return p
-print (my_sum(1,2,3,4,5,10, start = 10)) 
-print (my_sum(1,2,3,4,5,155, start = 0))
+print (my_sum(1,2,3,4,5,10, start=0 )) 
+print (my_sum(1,2,3,4,5,155, ))
