9ee777bf66435e3f83f3bdb237eb88b4ef82deff
MaxymVl <111682894+MaxymVl@users.noreply.github.com>
2022-09-08 17:16:30 +0300

    Update home_work3_1.py

--- home_work3/home_work3_1.py
+++ home_work3/home_work3_1.py
@@ -1,9 +1,11 @@
 from cmath import cos, pi
 import math
+
+deg = math.pi/180
 def degrees2radians(x):
     deg = x*math.pi/180
     y = math.cos(x*pi/180)
     return deg, y
-print (degrees2radians(60))
-print (degrees2radians(45))
-print (degrees2radians(40))
+print(degrees2radians(60))
+print(degrees2radians(45))
+print(degrees2radians(40))
