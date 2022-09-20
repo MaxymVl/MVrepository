c67186b42c4e85cb13bb57bd1ca2d7022d6a1a99
MaxymVl <111682894+MaxymVl@users.noreply.github.com>
2022-09-20 10:46:58 +0300

    Update home_work_4_3.py

--- home_work4/home_work_4_3.py
+++ home_work4/home_work_4_3.py
@@ -6,9 +6,9 @@ def calc_interception(x1, y1, r1, x2, y2, r2):
     
     cen_dist = sqrt((x1-x2)**2 + (y1-y2)**2)
     if cen_dist > (r1 + r2) or ((x1-x2)**2 + (y1-y2)**2) >= (r2+r1)**2:
-        return("окружности не пересекаются")
+        return False
     elif cen_dist == 0:
-        return("2 окружности имеют один и тот же центр")
+        return False
     else:
-        return('Окружности пересекаются')
+        return True
 print(calc_interception(4, 3, 2, 7, 5, 3))
