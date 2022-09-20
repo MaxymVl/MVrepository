12d7266f801b3bcad958b27ed9c150cba5a192e8
MaxymVl <111682894+MaxymVl@users.noreply.github.com>
2022-09-20 10:49:37 +0300

    Update home_work_4_4.py

--- home_work4/home_work_4_4.py
+++ home_work4/home_work_4_4.py
@@ -5,8 +5,8 @@ year = int(input("Введите любой год: "))
 def leap(year):
 
     if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):   #Leap year is a multiple of 4, but not a multiple of 100, or a multiple of 400
-        return ('YES')
+        return 'YES'
     else:
-        return('NO')
+        return 'NO'
 
 print(leap(year))    
