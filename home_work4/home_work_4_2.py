d56ad8625834ebb4a78d70275c7ce5f2e91ec6fd
MaxymVl <111682894+MaxymVl@users.noreply.github.com>
2022-09-20 11:33:00 +0300

    Update home_work_4_2.py

--- home_work4/home_work_4_2.py
+++ home_work4/home_work_4_2.py
@@ -4,13 +4,17 @@ from math import sqrt
 def solve_quadratic_equation(a,b,c):
     Discr = b**2 - 4*a*c             #  discriminant formula
 
-    if Discr >= 0:
+    if Discr > 0:
         x1 = (-b + sqrt(Discr)) / (2*a)        #  formula for determining the root of a quadratic equation
         x2 = (-b - sqrt(Discr)) / (2*a)        #  formula for determining the root of a quadratic equation
-        text = "Дискриминант: %s \n x1 = %s \n x2 = %s \n" % (Discr, x1, x2)        
+    elif Discr == 0:  
+        x1 = -b / 2*a 
+        x2 = None 
     else:
-        text = "Дискриминант: %s \n У уравнения решений нет" % Discr 
-    return text
+        x1 = None
+        x2 = None 
+    return x1, x2
 print(solve_quadratic_equation(-3,1,2))
 print(solve_quadratic_equation(-5,6,1))
 print(solve_quadratic_equation(5,6,1))
+print(solve_quadratic_equation(1,2,1))
