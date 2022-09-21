1a39183961e34924366ada2d6eb9d03151f1cc75
MaxymVl <111682894+MaxymVl@users.noreply.github.com>
2022-09-21 09:26:46 +0300

    Update home_work_4_5.py

--- home_work4/home_work_4_5.py
+++ home_work4/home_work_4_5.py
@@ -2,13 +2,13 @@
 
 number = int(input("Введите любое число: "))
 
-def sign_num(x):
+def sign(x):
 
-    if x>0:  
+    if x > 0:  
         return +1
-    elif x<0:
+    elif x < 0:
         return -1
     else:
         return 0
 
-print(sign_num(number)) 
+print(sign(number)) 
