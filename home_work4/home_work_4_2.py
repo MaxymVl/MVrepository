c520114e395c22afee7e96b0b0c4403ba1b68314
MaxymVl <111682894+MaxymVl@users.noreply.github.com>
2022-09-21 09:31:38 +0300

    Update home_work_4_2.py

--- home_work4/home_work_4_2.py
+++ home_work4/home_work_4_2.py
@@ -2,13 +2,13 @@
 from math import sqrt
 
 def solve_quadratic_equation(a,b,c):
-    Discr = b**2 - 4*a*c             #  discriminant formula
+    discr = b**2 - 4*a*c             #  discriminant formula
 
-    if Discr > 0:
-        x1 = (-b + sqrt(Discr)) / (2*a)        #  formula for determining the root of a quadratic equation
-        x2 = (-b - sqrt(Discr)) / (2*a)        #  formula for determining the root of a quadratic equation
-    elif Discr == 0:  
-        x1 = -b / 2*a 
+    if discr > 0:
+        x1 = (-b + sqrt(discr)) / (2*a)        #  formula for determining the root of a quadratic equation
+        x2 = (-b - sqrt(discr)) / (2*a)        #  formula for determining the root of a quadratic equation
+    elif discr == 0:  
+        x1 = -b / (2*a) 
         x2 = None 
     else:
         x1 = None
@@ -18,3 +18,4 @@ print(solve_quadratic_equation(-3,1,2))
 print(solve_quadratic_equation(-5,6,1))
 print(solve_quadratic_equation(5,6,1))
 print(solve_quadratic_equation(1,2,1))
+print(solve_quadratic_equation(2,-4,2))
