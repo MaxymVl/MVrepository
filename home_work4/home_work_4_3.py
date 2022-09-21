176198d6c68a679dd93d4632359578c9454e861d
MaxymVl <111682894+MaxymVl@users.noreply.github.com>
2022-09-21 09:22:31 +0300

    Update home_work_4_3.py

--- home_work4/home_work_4_3.py
+++ home_work4/home_work_4_3.py
@@ -2,13 +2,13 @@
 
 from math import sqrt
 
-def calc_interception(x1, y1, r1, x2, y2, r2):
-    
-    cen_dist = sqrt((x1-x2)**2 + (y1-y2)**2)
-    if cen_dist > (r1 + r2) or ((x1-x2)**2 + (y1-y2)**2) >= (r2+r1)**2:
-        return False
+def circles_intersect(x1, y1, r1, x2, y2, r2):
+    p = (x1-x2)**2 + (y1-y2)**2
+    cen_dist = sqrt(p)
+    if cen_dist > (r1 + r2) or p >= (r2+r1)**2:
+        return False 
     elif cen_dist == 0:
-        return False
+        return False 
     else:
         return True
-print(calc_interception(4, 3, 2, 7, 5, 3))
+print(circles_intersect(4, 3, 2, 7, 5, 3))
